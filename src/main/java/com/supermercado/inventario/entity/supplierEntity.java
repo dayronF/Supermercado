696df71431518d7db88746eea97b48d30a19623f
rodriguezsmith2008-web <rodriguez.smith.2008@gmail.com>
2026-03-31 13:30:14 -0500

    Feat:Creacion de la entidad proveedor

--- src/main/java/com/supermercado/inventario/entity/supplierEntity.java
+++ src/main/java/com/supermercado/inventario/entity/supplierEntity.java
@@ -1,5 +1,26 @@
 package com.supermercado.inventario.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "suppliers")
 public class supplierEntity {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    int id ;
+
+    @Column(name = "nit")
+    String nit;
+
+    @Column(name = "name")
+    String name;
+
 }
