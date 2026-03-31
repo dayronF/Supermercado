2307cf0b5fe2dd92baa4782cf0fa4db3ab08f271
rodriguezsmith2008-web <rodriguez.smith.2008@gmail.com>
2026-03-31 09:35:20 -0500

    Feat:Creacion de la entidad y aumento de deendencias e el pomp.xml

--- src/main/java/com/supermercado/inventario/entity/categoriesEntity.java
+++ src/main/java/com/supermercado/inventario/entity/categoriesEntity.java
@@ -1,5 +1,24 @@
 package com.supermercado.inventario.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+import jakarta.persistence.GenerationType;
+
+@Entity
+@Data
+@Table(name = "categories")
+
 public class categoriesEntity {
-    
+
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+int id;
+
+@Column(name = "name")
+String name;
+
 }
